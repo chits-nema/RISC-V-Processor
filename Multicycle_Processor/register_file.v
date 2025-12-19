9801519aad4f9645273dcd88b9ae4fd6aa20f586
Chitsidzo Varaidzo Nemazuwa <cvnemazuwa@gmail.com>
2025-12-19 01:10:59 +0100

    Refactor register 0 initialization in register_file.v
    Removed hardcoded initialization of register 0 and replaced it with an always block to ensure it remains 0.

--- Multicycle_Processor/register_file.v
+++ Multicycle_Processor/register_file.v
@@ -9,19 +9,17 @@ module regfile(
     output [31:0] rd2
     );
 
-    initial begin
-        //hardcode register 0 to 0
-        registers[0] = 32'b0;
-    end
-
     //32 registers because rd(a3) is 5 bits
     reg [31:0] registers [0:31];
+
 
     //register 0 is always 0
     assign rd1 = (a1 == 5'b00000) ? 32'b0 : registers[a1];
     assign rd2 = (a2 == 5'b00000) ? 32'b0 : registers[a2];
 
     always @(posedge clk) begin
+        registers[0] = 32'd0;
+
         //register 0 is read-only
         if (we_rf && (a3 != 5'b00000)) begin
             registers[a3] <= wd;
