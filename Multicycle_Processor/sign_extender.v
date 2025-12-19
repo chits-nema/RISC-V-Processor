6709bbe153319dad991687429ad2feb8234a9181
Chitsidzo Varaidzo Nemazuwa <cvnemazuwa@gmail.com>
2025-12-19 01:12:09 +0100

    Fix sign extension for S-type immediate

--- Multicycle_Processor/sign_extender.v
+++ Multicycle_Processor/sign_extender.v
@@ -11,14 +11,14 @@ module sign_extender(
             //I-type: 12-bit signed immediate -> alu i type instructions and lw
             3'b000: out = {{20{in[31]}}, in[31:20]}; //{a,b} is concatenation so result is a 32 bit value with a followed by b
             //S-type: 12-bit signed immediate -> sw instruction
-            3'b001: out = {{20{in[31:25]}},in[11:7]};
+            3'b001: out = {{20{in[31]}}, in[31:25], in[11:7]};
             //B-type: 13-bit signed immediate -> beq instruction
             3'b010: out = {{20{in[31]}}, in[7], in[30:25], in[11:8], 1'b0};
             //J-type: 21-bit signed immediate -> jal instruction
             3'b011: out = {{12{in[31]}}, in[19:12], in[20], in[30:21], 1'b0};
             //U-type: 20 bit immediate -> lui instruction
             3'b100: out = {in[31:12],12'b0};
-            default: $display("Invalid sel_ext value");
+            default: out = 32'b0;
         endcase
     end
 
